10b6f423c483fe8f5ca2d27e48db6d7140b7e24d
Sham-98 <130893083+Sham-98@users.noreply.github.com>
2023-04-25 20:29:09 +0300

    25.4.2023
    LutemonAdapter new class
    
    Co-Authored-By: 84765 <123329630+84765@users.noreply.github.com>
    Co-Authored-By: Destiniu <131793505+Destiniu@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/ListLutemonActivity.java
+++ app/src/main/java/org/gpiste/javaprojectapp/ListLutemonActivity.java
@@ -3,12 +3,27 @@ package org.gpiste.javaprojectapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListLutemonActivity extends AppCompatActivity {
+    private ListView mListView;
+    private LutemonAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nlist_lutemon);
+
+        mListView = findViewById(R.id.list_view);
+        mAdapter = new LutemonAdapter(this);
+
+        // Retrieve Lutemons from a SQLite database
+        List<Lutemon> lutemons = LutemonDatabase.getInstance(this).getLutemons();
+
+        // Add Lutemons to the adapter
+        mAdapter.addAll(lutemons);
+
+        // Set the adapter on the ListView
+        mListView.setAdapter(mAdapter);
     }
 }
