92b3a7700f881333ada27d272e912d2c163a3fd5
84765 <liisa.maria.hovilehto@gmail.com>
2023-04-26 14:43:32 +0300

    Layout korjaus: Annika
    Co-Authored-By: Destiniu <131793505+Destiniu@users.noreply.github.com>
    Co-Authored-By: Sham-98 <130893083+Sham-98@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/Black.java
+++ app/src/main/java/org/gpiste/javaprojectapp/Black.java
@@ -1,7 +1,7 @@
 package org.gpiste.javaprojectapp;
 
-public class Black extends Lutemon {
-    public Black(String name, String color) {
+public class Black {
+    /*public Black(String name, String color) {
         super(name, color);
-    }
+    }*/
 }
