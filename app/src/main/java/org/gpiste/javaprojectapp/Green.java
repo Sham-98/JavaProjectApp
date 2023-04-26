47818894636f78a633389ada84a4455a6a917584
Sham-98 <130893083+Sham-98@users.noreply.github.com>
2023-04-26 14:57:29 +0300

    Sham
    Green, White, Pink, and Orang can inherit
    
    Co-Authored-By: 84765 <123329630+84765@users.noreply.github.com>
    Co-Authored-By: Destiniu <131793505+Destiniu@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/Green.java
+++ app/src/main/java/org/gpiste/javaprojectapp/Green.java
@@ -1,4 +1,7 @@
 package org.gpiste.javaprojectapp;
 
-public class Green {
+public class Green extends Lutemon {
+    public Green(String name, String color, int attack, int defense, int experience, int health, int maxHealth, int id) {
+        super(name, color, 6, 3, experience, health, 19, id);
+    }
 }
--- app/src/main/java/org/gpiste/javaprojectapp/White.java
+++ app/src/main/java/org/gpiste/javaprojectapp/White.java
@@ -1,4 +1,7 @@
 package org.gpiste.javaprojectapp;
 
-public class White {
+public class White extends Lutemon {
+    public White(String name, String color, int attack, int defense, int experience, int health, int maxHealth, int id) {
+        super(name, color, 5, 4, experience, health, 20, id);
+    }
 }
