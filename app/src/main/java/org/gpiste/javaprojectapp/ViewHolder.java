f6828789c49d95754fb2a7a4534e7ecfde1f3eb1
Sham-98 <130893083+Sham-98@users.noreply.github.com>
2023-04-27 12:33:42 +0300

    Update ViewHolder.java
    updates
    
    Co-Authored-By: 84765 <123329630+84765@users.noreply.github.com>
    Co-Authored-By: Destiniu <131793505+Destiniu@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/ViewHolder.java
+++ app/src/main/java/org/gpiste/javaprojectapp/ViewHolder.java
@@ -1,19 +1,57 @@
 package org.gpiste.javaprojectapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ViewHolder extends RecyclerView.ViewHolder {
+import java.util.ArrayList;
 
-    public TextView Name;
+public class ListLutemonAdapter extends RecyclerView.Adapter<ListLutemonAdapter.ListLutemonViewHolder> {
 
-    public ViewHolder(@NonNull View lutemonView) {
-        super(lutemonView);
-        View txtName = lutemonView.findViewById(R.id.txtName);
+    private Context context;
+    private ArrayList<Lutemon> lutemons;
+    private LayoutInflater inflater;
+
+    public ListLutemonAdapter(Context context, ArrayList<Lutemon> lutemons) {
+        this.context = context;
+        this.lutemons = lutemons;
+        inflater = LayoutInflater.from(context);
     }
 
+    @NonNull
+    @Override
+    public ListLutemonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.lutemonview, parent, false);
+        return new ListLutemonViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListLutemonViewHolder holder, int position) {
+        Lutemon lutemon = lutemons.get(position);
+        holder.nameTextView.setText(lutemon.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public int getLutemonCount() {
+        return lutemons.size();
+    }
+
+    public class ListLutemonViewHolder extends RecyclerView.ViewHolder {
+        public TextView nameTextView;
+
+        public ListLutemonViewHolder(@NonNull View lutemonView) {
+            super(lutemonView);
+            nameTextView = lutemonView.findViewById(R.id.nameTextView);
+        }
+    }
 }
