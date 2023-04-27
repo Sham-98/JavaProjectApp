e69e19b7d229b1c2a849b8a698a09cc6503a8304
Sham-98 <130893083+Sham-98@users.noreply.github.com>
2023-04-27 22:08:06 +0300

    Update ListLutemonAdapter.java
    Co-Authored-By: 84765 <123329630+84765@users.noreply.github.com>
    Co-Authored-By: Destiniu <131793505+Destiniu@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/ListLutemonAdapter.java
+++ app/src/main/java/org/gpiste/javaprojectapp/ListLutemonAdapter.java
@@ -7,28 +7,40 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class ListLutemonAdapter extends RecyclerView.Adapter<ListLutemonAdapter.LutemonViewHolder> {
-    private final Context context;
-    private final List<Lutemon> lutemonList;
-    public ListLutemonAdapter(Context applicationContext, Object lutemonList) {
+public class ListLutemonAdapter extends RecyclerView.Adapter<ViewHolder> {
+    private Context context;
+    private ArrayList<Lutemon> lutemons = new ArrayList<>();
+
+    public ListLutemonAdapter(Context context, ArrayList<Lutemon> lutemons) {
         this.context = context;
-        this.lutemonList = lutemonList;
+        this.lutemons = lutemons;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.lutemonview, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    holder.Name.setText(lutemons.get(position).getName() + " (" + lutemons.get(position).getId()+ " (");
+    //holder.Color.setText(lutemons.get(position).getColor() + " (" + lutemons.get(position).getId()+ " (");
+    holder.Attack.setText(lutemons.get(position).getAttack() + " (" + lutemons.get(position).getId()+ " (");
+    holder.Defense.setText(lutemons.get(position).getDefense() + " (" + lutemons.get(position).getId()+ " (");
+    holder.Experience.setText(lutemons.get(position).getExperience() + " (" + lutemons.get(position).getId()+ " (");
+    holder.Health.setText(lutemons.get(position).getHealth() + " (" + lutemons.get(position).getId()+ " (");
+    //holder.Maxhealth.setText(lutemons.get(position).getName() + " (" + lutemons.get(position).getId()+ " (");holder.Name.setText(lutemons.get(position).getName() + " (" + lutemons.get(position).getId()+ " (");
+    //holder..setText(lutemons.get(position).getName() + " (" + lutemons.get(position).getId()+ " (");holder.Name.setText(lutemons.get(position).getName() + " (" + lutemons.get(position).getId()+ " (");
 
     }
 
     @Override
     public int getItemCount() {
-        return lutemonList.size();
+        return lutemons.size();
     }
+}
