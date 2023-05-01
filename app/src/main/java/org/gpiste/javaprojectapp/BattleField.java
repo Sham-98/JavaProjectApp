0e66ef9ab08cf8d5c56380a34b1154cbb612b1cc
84765 <liisa.maria.hovilehto@gmail.com>
2023-05-01 20:41:21 +0300

    Annika
    Battle
    
    Co-Authored-By: Destiniu <131793505+Destiniu@users.noreply.github.com>
    Co-Authored-By: Sham-98 <130893083+Sham-98@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/BattleField.java
+++ app/src/main/java/org/gpiste/javaprojectapp/BattleField.java
@@ -16,27 +16,21 @@ public class BattleField extends Storage {
         String battleResult = "";
         while (lutemonA.getHealth() > 0 && lutemonB.getHealth() > 0) {
 
-            /*battleResult += lutemonA.getName() + "(" + ") attack: " + lutemonA.getAttack() + "; def: "
+            battleResult += "1. " + lutemonA.getName() + "(" + lutemonA.getColor() + ") attack: " + lutemonA.getAttack() + "; def: "
                 + lutemonA.getDefense() + "; exp: " + lutemonA.getExperience() + "; health: " + lutemonA.getHealth()
-                + "/" + lutemonA.getMaxHealth() +"\n";*/
+                + "/" + lutemonA.getMaxHealth() +"\n";
 
-            System.out.println(lutemonA.getName() + "(" + ") attack: " + lutemonA.getAttack() + "; def: "
-                    + lutemonA.getDefense() + "; exp: " + lutemonA.getExperience() + "; health: " + lutemonA.getHealth()
-                    + "/" + lutemonA.getMaxHealth());
-
-            System.out.println(lutemonB.getName() + "(" + ") attack: " + lutemonB.getAttack() + "; def: "
+            battleResult += "2. " + lutemonB.getName() + "(" + lutemonB.getColor() + ") attack: " + lutemonB.getAttack() + "; def: "
                     + lutemonB.getDefense() + "; exp: " + lutemonB.getExperience() + "; health: " + lutemonB.getHealth()
-                    + "/" + lutemonB.getMaxHealth());
+                    + "/" + lutemonB.getMaxHealth() + "\n";
 
             int damage = lutemonA.getAttack() + lutemonA.getExperience();
             lutemonB.defende(damage);
 
-            //battleResult += lutemonA.getName() + " attacked " + lutemonB.getName() + " and dealt " + damage + " damage.\n";
-            System.out.println(lutemonA.getName() + " attacked " + lutemonB.getName() + " and dealt " + damage + " damage.");
+            battleResult += lutemonA.getName() + " attacked " + lutemonB.getName() + " and dealt " + damage + " damage.\n\n";
 
             if (lutemonB.getHealth() <= 0) {
                 battleResult += lutemonB.getName() + " died.\n";
-                System.out.println(lutemonB.getName() + " died.");
                 lutemonA.gainExperience(1);
                 getBattle().remove(lutemonB);
                 lutemonB = null;
--- app/src/main/java/org/gpiste/javaprojectapp/Green.java
+++ app/src/main/java/org/gpiste/javaprojectapp/Green.java
@@ -3,7 +3,7 @@ package org.gpiste.javaprojectapp;
 public class Green extends Lutemon {
 
     public Green(String name) {
-        super(name, "Green", 6, 3, 0, 0, 19, 2);
+        super(name, "Green", 6, 3, 0, 19, 19, 2);
 
         image = R.drawable.green;
     }
