3fced64c460e6075338aee44e359ff5c2db50b27
Destiniu <destina-03@hotmail.com>
2023-05-02 18:19:29 +0300

    Irem
    Suomennos
    
    Co-Authored-By: 84765 <123329630+84765@users.noreply.github.com>
    Co-Authored-By: Sham-98 <130893083+Sham-98@users.noreply.github.com>

--- app/src/main/java/org/gpiste/javaprojectapp/BattleField.java
+++ app/src/main/java/org/gpiste/javaprojectapp/BattleField.java
@@ -20,23 +20,25 @@ public class BattleField extends Storage {
             int damage = lutemonA.getAttack() + lutemonA.getExperience();
             int defense = lutemonB.getDefense();
 
-            battleResult += lutemonA.getName() + "(" + lutemonA.getColor() + ") attack: " + lutemonA.getAttack() + "; def: "
-                    + lutemonA.getDefense() + "; exp: " + lutemonA.getExperience() + "; health: " + lutemonA.getHealth()
+            battleResult += lutemonA.getName() + "(" + lutemonA.getColor() + ") hyökkäys: " + lutemonA.getAttack() + "; puolustus: "
+                    + lutemonA.getDefense() + "; kokemus: " + lutemonA.getExperience() + "; elämä: " + lutemonA.getHealth()
                     + "/" + lutemonA.getMaxHealth() +"\n";
 
-            battleResult += lutemonB.getName() + "(" + lutemonB.getColor() + ") attack: " + lutemonB.getAttack() + "; def: "
-                    + lutemonB.getDefense() + "; exp: " + lutemonB.getExperience() + "; health: " + lutemonB.getHealth()
+            battleResult += lutemonB.getName() + "(" + lutemonB.getColor() + ") hyökkäys: " + lutemonB.getAttack() + "; puolustus: "
+                    + lutemonB.getDefense() + "; kokemus: " + lutemonB.getExperience() + "; elämä: " + lutemonB.getHealth()
                     + "/" + lutemonB.getMaxHealth() + "\n";
 
             lutemonB.defende(damage, defense);
-            battleResult += lutemonA.getName() + " attacked " + lutemonB.getName() + " and dealt " + damage + " damage.\n\n";
+            battleResult += lutemonA.getName() + " hyökkää " + lutemonB.getName() + ":n kimppuun ja aiheuttaa " + damage + " pistettä vahinkoa.\n";
+
+            battleResult += lutemonB.getName() + " onnistui pakenemaan kuolemaa:\n\n";
 
             Lutemon temp = lutemonA;
             lutemonA = lutemonB;
             lutemonB = temp;
 
             if (lutemonB.getHealth() <= 0) {
-                battleResult += lutemonB.getName() + " died.\n";
+                battleResult += lutemonB.getName() + " kuoli.\n";
                 lutemonA.gainExperience(1);
                 getInstance().removeDeadLutemon(lutemonB);
                 getInstance().moveLutemon(lutemonA, getInstance().getBattle(), getInstance().getHome());
@@ -45,7 +47,7 @@ public class BattleField extends Storage {
             }
 
             if (lutemonA.getHealth() <= 0) {
-                battleResult += lutemonA.getName() + " died.\n";
+                battleResult += lutemonA.getName() + " kuoli.\n";
                 lutemonB.gainExperience(1);
                 getInstance().removeDeadLutemon(lutemonA);
                 getInstance().moveLutemon(lutemonB, getInstance().getBattle(), getInstance().getHome());
